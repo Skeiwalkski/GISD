85dedd4480216412cda0ec8a279876135a40ecd3
Reis <ReisM@rki.local>
2021-05-20 16:42:20 +0200

    Überarbeitung Karte GISD-Score

--- Generate_Shape_Gemeinden.R
+++ Generate_Shape_Gemeinden.R
@@ -1,11 +1,13 @@
 library(tidyverse)
 library(rgdal)
 
-BRD_Gemeinden <- readOGR(dsn = path.expand("S:/OE/FG28/205 Regionale Unterschiede/Referenzdaten/Kartendaten/BRD/Gemeinden/vg250_01-01.utm32s.shape.kompakt/vg250_kompakt_0101"), 
-                     layer = "VG250_P")
+BRD_Gemeinden <- readOGR(dsn = path.expand("S:/OE/FG28/205 Regionale Unterschiede/Referenzdaten/Kartendaten/BRD/2012/VG250-EW/vg250-ew_ebenen"), 
+                     layer = "vg250_gem")
 
 library(broom)
-BRD_Gemeinden_tidy <- tidy(BRD_Gemeinden)
+BRD_Gemeinden <- tidy(BRD_Gemeinden)
+
+saveRDS(BRD_Gemeinden, "C:/git_projects/GISD/BRD_Gemeinden.rds")
 
 ggplot(BRD_Gemeinden, aes(x = long, y = lat, group = group)) +
   geom_polygon(color = "black", size = 0.1, fill = "lightgrey") +
