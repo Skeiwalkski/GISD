1550fe59ba0f7e536d6015d84e041e7b1c5a941f
Niels VD <MichalskiN@rki.de>
2021-05-21 14:49:27 +0200

    add region = AGS to tidy

--- Generate_Shape_Gemeinden.R
+++ Generate_Shape_Gemeinden.R
@@ -1,13 +1,27 @@
 library(tidyverse)
 library(rgdal)
-
-BRD_Gemeinden <- readOGR(dsn = path.expand("S:/OE/FG28/205 Regionale Unterschiede/Referenzdaten/Kartendaten/BRD/Gemeinden/vg250_01-01.utm32s.shape.kompakt/vg250_kompakt_0101"), 
-                     layer = "VG250_P")
-
 library(broom)
-BRD_Gemeinden_tidy <- tidy(BRD_Gemeinden)
+require(sf)
+
+library(rgdal)
+library(maptools)
+if (!require(gpclib)) install.packages("gpclib", type="source")
+gpclibPermit()
+
+
+dsn <- "S:/OE/FG28/205 Regionale Unterschiede/Referenzdaten/Kartendaten/BRD/Gemeinden_VZ/vz250_01-01.utm32s.shape/vz250_0101"
+lay <- "VZ250_GEM"
+
+
+BRD_Gemeinden <- readOGR(dsn = dsn,                      
+                         layer = lay)
+
+BRD_Gemeinden_tidy <- tidy(BRD_Gemeinden, region = "AGS_G")
 
 ggplot(BRD_Gemeinden, aes(x = long, y = lat, group = group)) +
   geom_polygon(color = "black", size = 0.1, fill = "lightgrey") +
   coord_equal() +
   theme_minimal()
+
+
+
